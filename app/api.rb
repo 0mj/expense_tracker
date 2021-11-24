e228bc26c12aea9426b9204f0f96b3f967addfdd
Matt Jamison <thirdshiftvideo@gmail.com>
2021-11-24 13:45:15 -0500

    ledger

--- app/api.rb
+++ app/api.rb
@@ -2,15 +2,26 @@ require 'sinatra/base'
 require 'json'
 
 module ExpenseTracker
-    class API < Sinatra::Base
-        post '/expenses' do
-            JSON.generate( 'expense_id' => 42)
-        end
-
-        get '/expenses/:date' do
-            JSON.generate([])
-        end
-        
+  class API < Sinatra::Base
+    def initialize(ledger: Ledger.new)
+      @ledger = ledger
+      super()
     end
-    
+
+    post '/expenses' do
+      expense = JSON.parse(request.body.read)
+      result = @ledger.record(expense)
+
+      if result.success?
+        JSON.generate('expense_id' => result.expense_id)
+      else
+        status 422
+        JSON.generate('error' => result.error_message)
+      end
+    end
+
+    get '/expenses/:date' do
+      JSON.generate([])
+    end
+  end
 end
